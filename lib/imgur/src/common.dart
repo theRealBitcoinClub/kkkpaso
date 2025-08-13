3ebd3eb4a26e41a1bcdcf409f8597d5503529838
The Real Bitcoin Club <therealbitcoin.club@protonmail.com>
2025-08-13 06:57:08 -0400

    Revert "imgur"
    This reverts commit 92767e44f7296d3725b4045bc6a73a5f5ad51ae6.

--- lib/imgur/src/common.dart
+++
@@ -1,9 +0,0 @@
-library imgur.common;
-
-import 'package:json_annotation/json_annotation.dart';
-
-import 'models.dart';
-
-part 'common/response.dart';
-
-part 'common/types.dart';
--- lib/imgur/src/services/service.dart
+++
@@ -1,8 +0,0 @@
-part of imgur.services;
-
-/// A class which serve as a base for every service.
-class BaseService {
-  final Imgur client;
-
-  BaseService(this.client);
-}
